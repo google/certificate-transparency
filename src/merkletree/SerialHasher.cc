0e358c89d6aeeddb14f0fcef2ee91abfd2d40837
Emilia Kasper <ekasper@google.com>
2011-12-09 19:35:17 +0100

    Implement SHA-256 SerialHasher

--- src/merkletree/SerialHasher.cc
+++ src/merkletree/SerialHasher.cc
@@ -7,26 +7,24 @@ const size_t Sha256Hasher::kDigestSize = SHA256_DIGEST_LENGTH;
 
 Sha256Hasher::Sha256Hasher() : initialized_(false) {}
 
-Sha256Hasher::~Sha256Hasher() {}
-
 void Sha256Hasher::Reset() {
   SHA256_Init(&ctx_);
   initialized_ = true;
 }
 
 void Sha256Hasher::Update(const std::string &data) {
-  if (!initialized_) {
+  if (!initialized_)
     Reset();
-  }
+
   SHA256_Update(&ctx_, data.data(), data.size());
 }
 
-void Sha256Hasher::Final(std::string *digest) {
-  if (!initialized_) {
+std::string Sha256Hasher::Final() {
+  if (!initialized_)
     Reset();
-  }
+
   unsigned char hash[SHA256_DIGEST_LENGTH];
   SHA256_Final(hash, &ctx_);
-  digest->assign(reinterpret_cast<const char*>(hash), SHA256_DIGEST_LENGTH);
+  return std::string(reinterpret_cast<const char*>(hash), SHA256_DIGEST_LENGTH);
   initialized_ = false;
 }
--- src/merkletree/SerialHasher.h
+++ src/merkletree/SerialHasher.h
@@ -11,26 +11,22 @@ class SerialHasher {
   virtual size_t DigestSize() const = 0;
   virtual void Reset() = 0;
   virtual void Update(const std::string &data) = 0;
-  virtual void Final(std::string *digest) = 0;
+  virtual std::string Final() = 0;
 };
 
 class Sha256Hasher : public SerialHasher {
  public:
-  static const size_t kDigestSize;
-
   Sha256Hasher();
-  ~Sha256Hasher();
 
-  size_t DigestSize() const {
-    return kDigestSize;
-  }
+  size_t DigestSize() const { return kDigestSize; }
 
   void Reset();
   void Update(const std::string &data);
-  void Final(std::string *digest);
+  std::string Final();
 
  private:
   SHA256_CTX ctx_;
   bool initialized_;
+  static const size_t kDigestSize;
 };
 #endif
