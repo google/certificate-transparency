b005931872d50e9592f56dc0da2f5f8edc968678
Emilia Kasper <ekasper@google.com>
2013-08-06 16:29:40 +0200

    Implement get-entries

--- src/python/ct/client/log_client.py
+++ src/python/ct/client/log_client.py
@@ -1,5 +1,5 @@
+import base64
 import gflags
-import json
 import logging
 import requests
 import threading
@@ -10,8 +10,10 @@ from ct.proto import client_pb2
 
 FLAGS = gflags.FLAGS
 
-gflags.DEFINE_integer('probe_frequency_secs', 10*60,
+gflags.DEFINE_integer("probe_frequency_secs", 10*60,
                       "How often to probe the logs for updates")
+gflags.DEFINE_integer("entry_batch_size", 1000, "Maximum number of entries to "
+                      "attempt to fetch in one request")
 
 class Error(Exception):
     pass
@@ -19,66 +21,207 @@ class Error(Exception):
 class ClientError(Error):
     pass
 
-class LogClient(object):
-    """HTTP client for talking to a CT log."""
+class HTTPError(Error):
+    """Connection failed, or returned an error."""
+    pass
 
-    get_sth_path = "ct/v1/get-sth"
+class HTTPClientError(HTTPError):
+    """HTTP 4xx."""
+    pass
 
+class HTTPServerError(HTTPError):
+    """HTTP 5xx."""
+    pass
+
+class InvalidRequestError(Error):
+    """Request does not comply with the CT protocol."""
+    pass
+
+class InvalidResponseError(Error):
+    """Response does not comply with the CT protocol."""
+    pass
+
+# requests.models.Response is not easily instantiable locally, so as a
+# workaround, encapsulate the entire http logic in the Requester class which we
+# can control/mock out to test response handling.
+class Requester(object):
     def __init__(self, uri):
-        self.uri = uri
+        self.__uri = uri
 
     def __repr__(self):
-        return "%r(%r)" % (self.__class__.__name__, self.uri)
+        return "%r(%r)" % (self.__class__.__name__, self.__uri)
 
     def __str__(self):
-        return "%s(%s)" % (self.__class__.__name__, self.uri)
+        return "%r(%r)" % (self.__class__.__name__, self.__uri)
 
     @property
-    def servername(self):
-        return self.uri
+    def uri(self):
+        return self.__uri
 
-    def _get_request(self, path, params={}):
-        """GET <logserver>/path?params."""
-        url = "https://" + self.uri + "/" + path
+    def get_json_response(self, path, params={}):
+        """Get the json contents of a request response."""
+        url = "https://" + self.__uri + "/" + path
         try:
             response = requests.get(url, params=params, timeout=60)
         except requests.exceptions.RequestException as e:
-            logging.error("Connection to %s failed: %s", url, e)
-            return
-
-        if response.status_code == 200:
+            raise HTTPError("Connection to %s failed: %s" % (url, e))
+        if not response.ok:
+            error_msg = ("%s returned http_error %d: %s" %
+                         (url, response.status_code, response.text))
+            if 400 <= response.status_code  < 500:
+                raise HTTPClientError(error_msg)
+            elif 500 <= response.status_code < 600:
+                raise HTTPServerError(error_msg)
+            else:
+                raise HTTPError(error_msg)
+        try:
             return response.json()
-        else:
-            # TODO(ekasper): handle JSON error responses.
-            logging.error("%s returned http error %d: %s", url,
-                          response.status_code, response.text)
+        # This can raise a variety of undocumented exceptions...
+        except e:
+            raise InvalidResponseError("Response %s from %s is not valid JSON: "
+                                       "%s" % (response, url, e))
+
+class LogClient(object):
+    """HTTP client for talking to a CT log."""
+
+    _GET_STH_PATH = "ct/v1/get-sth"
+    _GET_ENTRIES_PATH = "ct/v1/get-entries"
+
+    def __init__(self, requester):
+        self.__req = requester
+
+    def __repr__(self):
+        return "%r(%r)" % (self.__class__.__name__, self.__req)
+
+    def __str__(self):
+        return "%s(%s)" % (self.__class__.__name__, self.__req.uri)
+
+    @property
+    def servername(self):
+        return self.__req.uri
 
     def get_sth(self):
-        """Get the current Signed Tree Head."""
-        sth = self._get_request(LogClient.get_sth_path)
-        if not sth:
-            return
+        """Get the current Signed Tree Head.
+        Returns: a ct.proto.client_pb2.SthResponse proto
+        Raises:
+            HTTPError, HTTPClientError, HTTPServerError: connection failed.
+                For logs that honour HTTP status codes, HTTPClientError (a 4xx)
+                should never happen.
+            InvalidResponseError: server response is invalid for the given
+                                  request.
+        """
+        sth = self.__req.get_json_response(self._GET_STH_PATH)
         sth_response = client_pb2.SthResponse()
         try:
-            sth_response.timestamp = sth['timestamp']
-            sth_response.tree_size = sth['tree_size']
-            sth_response.sha256_root_hash = sth[
-                'sha256_root_hash'].decode("base64")
-            sth_response.tree_head_signature = sth[
-                'tree_head_signature'].decode("base64")
-        except:
-            logging.error("%s returned invalid JSON: %s", self.uri, sth)
-            return
-
+            sth_response.timestamp = sth["timestamp"]
+            sth_response.tree_size = sth["tree_size"]
+            sth_response.sha256_root_hash = base64.b64decode(sth[
+                "sha256_root_hash"])
+            sth_response.tree_head_signature = base64.b64decode(sth[
+                "tree_head_signature"])
+        # TypeError for base64 decoding, TypeError/ValueError for invalid
+        # JSON field types, KeyError for missing JSON fields.
+        except (TypeError, ValueError, KeyError) as e:
+            raise InvalidResponseError("%s returned an invalid STH %s\n%s" %
+                                       (self.__req.uri, sth, e))
         return sth_response
 
+    def __json_entry_to_response(self, json_entry):
+        """Convert a json array element to an EntryResponse."""
+        entry_response = client_pb2.EntryResponse()
+        try:
+            entry_response.leaf_input = base64.b64decode(
+                json_entry["leaf_input"])
+            entry_response.extra_data = base64.b64decode(
+                json_entry["extra_data"])
+        except (TypeError, ValueError, KeyError) as e:
+            raise InvalidResponseError(
+                "%s returned invalid data: expected a log entry, got %s"
+                "\n%s" % (self.__req.uri, json_entry, e))
+        return entry_response
+
+    def __validated_entry_response(self, start, end, response):
+        """Verify the get-entries response format and size. Returns an array
+        of entries."""
+        entries = None
+        try:
+            entries = iter(response["entries"])
+        except (TypeError, KeyError) as e:
+            raise InvalidResponseError("%s returned invalid data: expected "
+                                       "an array of entries, got %s\n%s)" %
+                                       (self.__req.uri, response, e))
+        expected_response_size = end - start + 1
+        response_size = len(response["entries"])
+        # Logs MAY honor requests where 0 <= "start" < "tree_size" and
+        # "end" >= "tree_size" by returning a partial response covering only
+        # the valid entries in the specified range.
+        # Logs MAY restrict the number of entries that can be retrieved per
+        # "get-entries" request.  If a client requests more than the
+        # permitted number of entries, the log SHALL return the maximum
+        # number of entries permissible. (RFC 6962)
+        #
+        # Therefore, we cannot assume we get exactly the expected number of
+        # entries. However if we get none, or get more than expected, then
+        # we discard the response and raise.
+        if not response_size or response_size > expected_response_size:
+            raise InvalidResponseError(
+                "%s returned invalid data: requested %d entries, got %d "
+                "entries" % (self.__req.uri, expected_response_size,
+                             response_size))
+
+        # If any one of the entries has invalid json format, this raises.
+        return map(lambda e: self.__json_entry_to_response(e), entries)
+
+    def get_entries(self, start, end, batch_size=FLAGS.entry_batch_size):
+        """Retrieve log entries.
+        Args:
+            start     : index of first entry to retrieve.
+            end       : index of last entry to retrieve.
+            batch_size: max number of entries to fetch in one go.
+        Yields:
+            ct.proto.client_pb2.EntryResponse protos.
+        Raises:
+            HTTPError, HTTPClientError, HTTPServerError: connection failed,
+                or returned an error. HTTPClientError can happen when
+                [start, end] is not a valid range for this log.
+            InvalidRequestError: invalid request range (irrespective of log).
+            InvalidResponseError: server response is invalid for the given
+                                  request
+        Caller is responsible for ensuring that (start, end) is a valid range
+        (by retrieving an STH first), otherwise a HTTPClientError may occur.
+        """
+        # Catch obvious mistakes here.
+        if start < 0 or end < 0 or start > end:
+            raise InvalidRequestError("Invalid range [%d, %d]" % (start, end))
+
+        while start <= end:
+            # Note that an HTTPError may occur here if the log does not have the
+            # requested range of entries available. RFC 6962 says:
+            # "Any errors will be returned as HTTP 4xx or 5xx responses, with
+            # human-readable error messages."
+            # There is thus no easy way to distinguish this case from other
+            # errors.
+            first = start
+            last = min(start + batch_size - 1, end)
+            response = self.__req.get_json_response(
+                self._GET_ENTRIES_PATH, params={"start": first, "end": last})
+            valid_entries = self.__validated_entry_response(first, last,
+                                                            response)
+            for entry in valid_entries:
+                yield entry
+            # If we got less entries than requested, then we don't know whether
+            # the log imposed a batch limit or ran out of entries, so we keep
+            # trying until we get all entries, or an error response.
+            start += len(valid_entries)
+
+# TODO(ekasper): move to another module.
 class LogProber(object):
     def __init__(self, ctlog):
         """Initialize from a CtLogMetadata proto."""
         if ctlog.log_server is None:
             raise ClientError("Cannot initialize log client: "
                               "no server URI given.")
-        self.client = LogClient(ctlog.log_server)
+        self.client = LogClient(Requester(ctlog.log_server))
         self.verifier = None
         if ctlog.public_key_info is not None:
             self.verifier = verify.LogVerifier(ctlog.public_key_info)
@@ -91,7 +234,7 @@ class LogProber(object):
 
     def __str__(self):
         ret = "%s: " % self.__class__.__name__
-        ret.append(' '.join([str(c) for c in self.clients]))
+        ret.append(" ".join([str(c) for c in self.clients]))
         return ret
 
     @property
@@ -99,9 +242,13 @@ class LogProber(object):
         return self.client.servername
 
     def probe_sth(self):
-        sth_response = self.client.get_sth()
-        if sth_response is None:
-            return None
+        try:
+            sth_response = self.client.get_sth()
+        except (HTTPError, InvalidResponseError) as e:
+           # TODO(ekasper): export stats about probe failures.
+           logging.error("Probing %s failed: %s" % (self.servername, e))
+           return
+
         audited_sth = client_pb2.AuditedSth()
         audited_sth.sth.CopyFrom(sth_response)
         audited_sth.audit.status = client_pb2.UNVERIFIED
@@ -150,7 +297,7 @@ class ProberThread(threading.Thread):
                 # unverified data.
                 self.db.store_sth(prober.servername, audited_sth)
             else:
-                logging.error("No valid response from %s", prober.servername)
+                logging.error("No valid response from %s" % prober.servername)
 
     def run(self):
         while not self.stopped:
---
+++ src/python/ct/client/log_client_test.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+
+import gflags
+import mock
+import requests
+import sys
+import unittest
+
+from ct.client import log_client
+from ct.proto import client_pb2
+
+FLAGS = gflags.FLAGS
+
+# TODO(ekasper): test the Requester class itself with a simple http server.
+
+class LogClientTest(unittest.TestCase):
+    # Generate valid-looking responses.
+    # We use this class to test the client handles valid responses correctly,
+    # and a Mock to test invalid responses.
+    class FakeResponder(log_client.Requester):
+        def __init__(self, entry_limit=0, log_size_limit=0):
+            log_client.Requester.__init__(self, "some_address")
+            self.__entry_limit = entry_limit
+            self.__log_size_limit = log_size_limit
+
+        def get_json_response(self, path, params={}):
+            if path == "ct/v1/get-sth":
+                return LogClientTest.sth_to_json(LogClientTest._DEFAULT_STH)
+            elif path == "ct/v1/get-entries":
+                start = params.get("start", -1)
+                end = params.get("end", -1)
+                if self.__log_size_limit > 0:
+                    end = min(end, self.__log_size_limit - 1)
+                if start < 0 or end < 0 or start > end:
+                    raise log_client.HTTPClientError("Bad params")
+                entries = []
+                if self.__entry_limit > 0:
+                    end = min(start + self.__entry_limit - 1, end)
+                return LogClientTest.entries_to_json(
+                    LogClientTest.make_entries(start, end))
+            else:
+                raise log_client.HTTPError("Bad path %s" % path)
+
+    _DEFAULT_STH = client_pb2.SthResponse()
+    _DEFAULT_STH.timestamp = 1234
+    _DEFAULT_STH.tree_size = 1000
+    _DEFAULT_STH.sha256_root_hash = "hash\x00"
+    _DEFAULT_STH.tree_head_signature = "sig\xff"
+
+    @staticmethod
+    def make_entries(start, end):
+        entries = []
+        for i in range(start, end+1):
+            entry = client_pb2.EntryResponse()
+            entry.leaf_input = "leaf_input-%d" % i
+            entry.extra_data = "extra_data-%d" % i
+            entries.append(entry)
+        return entries
+
+    def verify_entries(self, entries, start, end):
+        self.assertEqual(end-start+1, len(entries))
+        for i in range(start, end+1):
+            self.assertEqual("leaf_input-%d" % i, entries[i].leaf_input)
+            self.assertEqual("extra_data-%d" % i, entries[i].extra_data)
+
+    @staticmethod
+    def sth_to_json(sth):
+        return {"timestamp": sth.timestamp, "tree_size": sth.tree_size,
+                "sha256_root_hash": sth.sha256_root_hash.encode("base64"),
+                "tree_head_signature": sth.tree_head_signature.encode("base64")}
+    @staticmethod
+    def entries_to_json(entries):
+        return {"entries": [{"leaf_input": entry.leaf_input.encode("base64"),
+                            "extra_data": entry.extra_data.encode("base64")}
+                            for entry in entries]}
+
+    def test_get_sth(self):
+        client = log_client.LogClient(self.FakeResponder())
+        sth_response = client.get_sth()
+
+
+        self.assertEqual(sth_response.timestamp, self._DEFAULT_STH.timestamp)
+        self.assertEqual(sth_response.tree_size, self._DEFAULT_STH.tree_size)
+        self.assertEqual(sth_response.sha256_root_hash,
+                         self._DEFAULT_STH.sha256_root_hash)
+        self.assertEqual(sth_response.tree_head_signature,
+                         self._DEFAULT_STH.tree_head_signature)
+
+    def test_get_sth_raises_on_invalid_response(self):
+        json_sth = self.sth_to_json(self._DEFAULT_STH)
+        json_sth.pop("timestamp")
+        mock_request = mock.Mock()
+        mock_request.get_json_response.return_value = json_sth
+        client = log_client.LogClient(mock_request)
+        self.assertRaises(log_client.InvalidResponseError, client.get_sth)
+
+    def test_get_sth_raises_on_invalid_base64(self):
+        json_sth = self.sth_to_json(self._DEFAULT_STH)
+        json_sth["tree_head_signature"] = "garbagebase64^^^"
+        mock_request = mock.Mock()
+        mock_request.get_json_response.return_value = json_sth
+        client = log_client.LogClient(mock_request)
+        self.assertRaises(log_client.InvalidResponseError, client.get_sth)
+
+    def test_get_entries(self):
+        client = log_client.LogClient(self.FakeResponder())
+        returned_entries = list(client.get_entries(0, 9))
+        self.verify_entries(returned_entries, 0, 9)
+
+    def test_get_entries_raises_on_invalid_response(self):
+        json_entries = self.entries_to_json(self.make_entries(4, 4))
+        json_entries["entries"][0].pop("leaf_input")
+        mock_request = mock.Mock()
+        mock_request.get_json_response.return_value = json_entries
+
+        client = log_client.LogClient(mock_request)
+        entries = client.get_entries(4, 4)
+        self.assertRaises(log_client.InvalidResponseError,
+                          entries.next)
+
+    def test_get_entries_raises_immediately_on_invalid_base64(self):
+        json_entries = self.entries_to_json(self.make_entries(3, 4))
+        json_entries["entries"][1]["leaf_input"] = "garbagebase64^^^"
+        mock_request = mock.Mock()
+        mock_request.get_json_response.return_value = json_entries
+
+        client = log_client.LogClient(mock_request)
+        entries = client.get_entries(3, 4)
+        # We shouldn't see anything, even if the first entry appeared valid.
+        self.assertRaises(log_client.InvalidResponseError,
+                          entries.next)
+
+    def test_get_entries_raises_on_empty_response(self):
+        mock_request = mock.Mock()
+        mock_request.get_json_response.return_value = self.entries_to_json([])
+
+        client = log_client.LogClient(mock_request)
+        entries = client.get_entries(4, 4)
+        self.assertRaises(log_client.InvalidResponseError,
+                          entries.next)
+
+    def test_get_entries_raises_on_too_large_response(self):
+        mock_request = mock.Mock()
+        mock_request.get_json_response.return_value = self.entries_to_json(
+            self.make_entries(4, 5))
+
+        client = log_client.LogClient(mock_request)
+        entries = client.get_entries(4, 4)
+        self.assertRaises(log_client.InvalidResponseError,
+                          entries.next)
+
+    def test_get_entries_returns_all_in_batches(self):
+        client = log_client.LogClient(self.FakeResponder())
+        returned_entries = list(client.get_entries(0, 9, batch_size=4))
+        self.verify_entries(returned_entries, 0, 9)
+
+    def test_get_entries_returns_all_for_limiting_server(self):
+        client = log_client.LogClient(self.FakeResponder(entry_limit=3))
+        returned_entries = list(client.get_entries(0, 9))
+        self.verify_entries(returned_entries, 0, 9)
+
+    def test_get_entries_returns_partial_if_log_returns_partial(self):
+        client = log_client.LogClient(self.FakeResponder(log_size_limit=3))
+        entries = client.get_entries(0, 9)
+        partial = []
+        for i in range(3):
+            partial.append(entries.next())
+        self.verify_entries(partial, 0, 2)
+        self.assertRaises(log_client.HTTPClientError, entries.next)
+
+if __name__ == "__main__":
+    sys.argv = FLAGS(sys.argv)
+    unittest.main()
