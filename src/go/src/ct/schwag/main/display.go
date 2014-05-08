8f63d2292eab8823c87b7683c149ebe012f834b8
Al Cutter <al@google.com>
2014-05-08 13:33:17 +0100

    Add a driver for the CT Hackday schwag

---
+++ src/go/src/ct/schwag/main/display.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"ct/client"
+	"ct/schwag"
+	"flag"
+)
+
+var logUri = flag.String("log_uri", "http://ct.googleapis.com/pilot", "CT Log base URI")
+var logName = flag.String("log_name", "Pilot", "Name to display on LCD")
+var fetchSecs = flag.Int("fetch_secs", 60, "Fetch a new STH every n seconds")
+var serialDevice = flag.String("serial_device", "/dev/ttyACM0", "Which serial device has the LCD connected")
+var serialBaud = flag.Int("serial_baud", 115200, "Speed at which to communicate with LCD device")
+var freshAge = flag.Int("fresh_age", 30*60, "STH age in seconds below which the display is completely green")
+var staleAge = flag.Int("stale_age", 90*60, "STH age in seconds after which the display is completely red")
+
+func main() {
+	flag.Parse()
+	logClient := client.New(*logUri)
+	displayConfig := schwag.DisplayConfig{
+		Client:    logClient,
+		Name:      *logName,
+		Port:      *serialDevice,
+		Baud:      *serialBaud,
+		FreshAge:  *freshAge,
+		StaleAge:  *staleAge,
+		FetchSecs: *fetchSecs,
+	}
+	display, err := schwag.NewLCDDisplay(displayConfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	display.Run()
+}
