2ff1cd0c5400c5681ab431a143e50a3bed4e64f4
Ruslan Kovalov <ruslan.kovalyov@gmail.com>
2014-06-10 21:38:49 +0200

    Extend HTTPLogClient functionality.

--- java/src/org/certificatetransparency/ctlog/comm/HttpPostInvoker.java
+++ java/src/org/certificatetransparency/ctlog/comm/HttpPostInvoker.java
@@ -1,12 +1,18 @@
 package org.certificatetransparency.ctlog.comm;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.IOException;
+import com.sun.org.apache.xml.internal.utils.URI;
 
 /**
  * Simple delegator to HttpClient, so it can be mocked
@@ -27,6 +33,27 @@ public class HttpPostInvoker {
       return httpClient.execute(post, new BasicResponseHandler());
     } catch (IOException e) {
       throw new LogCommunicationException("Error making POST request to " + url, e);
+    }
+  }
+  
+  /**
+   * Makes an HTTP GET method call to the given URL with the provides parameters.
+   * @param ctUrl URL for GET method.
+   * @param params query parameters.
+   * @return Server's response body.
+   */
+  public String makeGetRequest(String url, List<NameValuePair> params) {
+    try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+      URI path = new URI(url);
+      if (params != null) {
+        path.setQueryString(URLEncodedUtils.format(params, "utf-8"));
+      }
+      HttpGet get = new HttpGet(path.toString());
+      get.addHeader("Content-Type", "application/json; charset=utf-8");
+
+      return httpClient.execute(get, new BasicResponseHandler());
+    } catch (IOException e) {
+      throw new LogCommunicationException("Error making GET request to " + url, e);
     }
   }
 }
