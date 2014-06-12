97036423ff0f30166b40eff724578b17a9a72a30
rep <ms@mwcollect.org>
2014-06-12 09:31:00 +0200

    update setup.py for it to actually work (sorry!)

--- python/setup.py
+++ python/setup.py
@@ -6,5 +6,5 @@ setup(name='Google Certificate Transparency',
       version='0.9',
       description='Certificate Transparency python client and monitor library',
       url='https://github.com/google/certificate-transparency',
-      packages=['ct'],
+      packages=['ct', 'ct.crypto', 'ct.crypto.asn1', 'ct.client'],
      )
