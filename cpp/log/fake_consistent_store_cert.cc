bada90d96a7f94577b0c4d7fc998fa63ab5fdece
Al Cutter <al@google.com>
2014-12-17 10:08:17 +0000

    Delete FakeConsistentStore source

--- cpp/log/fake_consistent_store_cert.cc
+++
@@ -1,6 +0,0 @@
-#include "log/fake_consistent_store-inl.h"
-#include "log/logged_certificate.h"
-
-namespace cert_trans {
-template class FakeConsistentStore<LoggedCertificate>;
-}  // namespace cert_trans
