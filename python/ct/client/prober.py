ef08f265cabe9a2659d79b6c190c4495021647bc
Konrad Kraszewski <laiquendir@gmail.com>
2015-01-09 10:24:55 +0000

    Added option to supply other Agent to Prober.
    Also fixed repr, and monitor overshadowing.

--- python/ct/client/prober.py
+++ python/ct/client/prober.py
@@ -18,19 +18,22 @@ from twisted.web import client as twisted_client
 
 class ProberThread(threading.Thread):
     """A prober for scheduled updating of the log view."""
-    def __init__(self, ct_logs, db, cert_db, temp_db_factory, monitor_state_dir):
+    def __init__(self, ct_logs, db, cert_db, temp_db_factory, monitor_state_dir,
+                 agent=None):
         """Initialize from a CtLogs proto."""
         threading.Thread.__init__(self)
 
         self.__monitors = []
         self.__db = db
+        if not agent:
+            agent = twisted_client.Agent(reactor)
         for log in ct_logs.ctlog:
             if not log.log_server or not log.log_id or not log.public_key_info:
                 raise RuntimeError("Cannot start monitor: log proto has "
                                    "missing or empty fields: %s" % log)
 
             temp_db = temp_db_factory.create_storage(log.log_server)
-            client = log_client.AsyncLogClient(twisted_client.Agent(reactor),
+            client = log_client.AsyncLogClient(agent,
                                                log.log_server,
                                                temp_db)
             hasher = merkle.TreeHasher()
@@ -46,7 +49,8 @@ class ProberThread(threading.Thread):
         self.__stopped = False
 
     def __repr__(self):
-        ret = "%r(%r)" % (self.__class__.__name__, self.__monitors)
+       return "%r(%r)" % (self.__class__.__name__, self.__monitors)
+
     def __str__(self):
        return "%s(%s)" % (self.__class__.__name__, self.__monitors)
 
@@ -79,9 +83,9 @@ class ProberThread(threading.Thread):
         self.__start_time = time.time()
         self.__probed = 0
         """Loop through all logs in the list and check for updates."""
-        for monitor in self.__monitors:
-            monitor_result = monitor.update()
-            monitor_result.addCallback(self._log_probed_callback, monitor)
+        for monitor_ in self.__monitors:
+            monitor_result = monitor_.update()
+            monitor_result.addCallback(self._log_probed_callback, monitor_)
 
     def run(self):
         reactor.callLater(0, self.probe_all_logs)
