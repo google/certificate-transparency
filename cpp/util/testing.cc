28e9e323a0b3e5f341a48c73170d17b38458f8ef
Pierre Phaneuf <pphaneuf@google.com>
2015-01-13 13:21:29 +0000

    Stop overriding the logging behaviour in tests.

--- cpp/util/testing.cc
+++ cpp/util/testing.cc
@@ -10,11 +10,6 @@ namespace test {
 
 void InitTesting(const char* name, int* argc, char*** argv,
                  bool remove_flags) {
-  // Change the defaults. Can be overridden on command line.
-  // Log to stderr instead of log files.
-  FLAGS_logtostderr = true;
-  // Only log fatal messages by default.
-  FLAGS_minloglevel = 3;
   ::testing::InitGoogleTest(argc, *argv);
   google::ParseCommandLineFlags(argc, argv, remove_flags);
   google::InitGoogleLogging(name);
