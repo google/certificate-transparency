e5b9df81a2b8bbae6a66d395a75c3c6bf257675b
Pierre Phaneuf <pphaneuf@google.com>
2015-02-10 14:35:33 +0000

    Make TreeHasher thread-safe.
    Also, a few other cleanups: formatting, hiding constants away, improved
    const-correctness, etc...

--- cpp/merkletree/tree_hasher.cc
+++ cpp/merkletree/tree_hasher.cc
@@ -1,41 +1,42 @@
 #include "merkletree/tree_hasher.h"
 
+#include <glog/logging.h>
+
 #include "merkletree/serial_hasher.h"
 
+using std::lock_guard;
+using std::mutex;
 using std::string;
 
-const string TreeHasher::kLeafPrefix(1, '\x00');
-const string TreeHasher::kNodePrefix(1, '\x01');
+namespace {
 
-TreeHasher::TreeHasher(SerialHasher* hasher) : hasher_(hasher) {
+const char kLeafPrefix('\x00');
+const char kNodePrefix('\x01');
+
+std::string EmptyHash(SerialHasher* hasher) {
+  hasher->Reset();
+  return hasher->Final();
 }
 
-TreeHasher::~TreeHasher() {
-  delete hasher_;
-}
+}  // namespace
 
-string TreeHasher::HashEmpty() {
-  if (emptyhash_.empty()) {
-    // First call to HashEmpty(); since the hash of an empty string is
-    // constant,
-    // set it up once and for all.
-    hasher_->Reset();
-    emptyhash_ = hasher_->Final();
-  }
-  return emptyhash_;
+TreeHasher::TreeHasher(SerialHasher* hasher)
+    : hasher_(CHECK_NOTNULL(hasher)), empty_hash_(EmptyHash(hasher_.get())) {
 }
 
 string TreeHasher::HashLeaf(const string& data) const {
+  lock_guard<mutex> lock(lock_);
   hasher_->Reset();
-  hasher_->Update(kLeafPrefix);
+  hasher_->Update(string(1, kLeafPrefix));
   hasher_->Update(data);
   return hasher_->Final();
 }
 
 string TreeHasher::HashChildren(const string& left_child,
-                                const string& right_child) {
+                                const string& right_child) const {
+  lock_guard<mutex> lock(lock_);
   hasher_->Reset();
-  hasher_->Update(kNodePrefix);
+  hasher_->Update(string(1, kNodePrefix));
   hasher_->Update(left_child);
   hasher_->Update(right_child);
   return hasher_->Final();
