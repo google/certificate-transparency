7a2ba57d88822e2f6ea73b7c96f70ac4819e4433
Pierre Phaneuf <pphaneuf@google.com>
2015-03-03 17:30:56 +0000

    Remove the unused HttpRequest and HttpConnection classes.

--- cpp/util/libevent_wrapper.h
+++ cpp/util/libevent_wrapper.h
@@ -21,7 +21,6 @@ namespace libevent {
 
 
 class Event;
-class HttpConnection;
 
 
 class Base : public util::Executor {
@@ -110,102 +109,6 @@ class HttpServer {
   std::vector<Handler*> handlers_;
 
   DISALLOW_COPY_AND_ASSIGN(HttpServer);
-};
-
-
-class HttpRequest : public std::enable_shared_from_this<HttpRequest> {
- public:
-  typedef std::function<void(const std::shared_ptr<HttpRequest>&)> Callback;
-
-  // Once this callback returns, the object becomes invalid.
-  explicit HttpRequest(const Callback& callback);
-  ~HttpRequest();
-
-  // After calling this, the object becomes invalid, and any reference
-  // to it should be disposed of. If it is too late to cancel and the
-  // callback is still running, this method will block until the
-  // callback has returned.
-  void Cancel();
-
-  int GetResponseCode() const {
-    return evhttp_request_get_response_code(req_);
-  }
-  evkeyvalq* GetInputHeaders() const {
-    return evhttp_request_get_input_headers(req_);
-  }
-  evbuffer* GetInputBuffer() const {
-    return evhttp_request_get_input_buffer(req_);
-  }
-  evkeyvalq* GetOutputHeaders() const {
-    return evhttp_request_get_output_headers(req_);
-  }
-  evbuffer* GetOutputBuffer() const {
-    return evhttp_request_get_output_buffer(req_);
-  }
-
- private:
-  friend class HttpConnection;
-
-  // Called by HttpConnection.
-  void Start(const std::shared_ptr<HttpConnection>& conn, evhttp_cmd_type type,
-             const std::string& uri);
-
-  static void Done(evhttp_request* req, void* userdata);
-  static void Cancelled(evutil_socket_t sock, short flag, void* userdata);
-
-  const Callback callback_;
-
-  evhttp_request* req_;
-  // We keep a reference to the HttpConnection as long as this request
-  // is outstanding, to make sure it doesn't disappear from under us.
-  std::shared_ptr<HttpConnection> conn_;
-
-  // A self-reference to keep the request object alive, as long as
-  // it's running.
-  std::shared_ptr<HttpRequest> self_ref_;
-
-  // This must be a recursive mutex, because libevent sometimes calls
-  // the completion callback synchronously, in some error cases. With
-  // an std::mutex, the Start() method would deadlock the Done()
-  // method, when really, we mainly want to protect against other
-  // threads concurrently accessing the object.
-  std::recursive_mutex cancel_lock_;
-  event* cancel_;
-  bool cancelled_;
-
-  DISALLOW_COPY_AND_ASSIGN(HttpRequest);
-};
-
-
-class HttpConnection : public std::enable_shared_from_this<HttpConnection> {
- public:
-  HttpConnection(const std::shared_ptr<Base>& base, const evhttp_uri* uri);
-  ~HttpConnection();
-
-  // This method does not simply clone the object, but creates a
-  // separate socket altogether. This can be useful for "hanging
-  // GETs", for example, which would otherwise prevent other requests
-  // from being made on the connection.
-  std::shared_ptr<HttpConnection> Clone() const;
-
-  // Once you pass an HttpRequest to this method, you shouldn't call
-  // any of its methods (except for HttpRequest::Cancel), until the
-  // callback is called.
-  void MakeRequest(const std::shared_ptr<HttpRequest>& req,
-                   evhttp_cmd_type type, const std::string& uri);
-
-  void SetTimeout(const std::chrono::seconds& timeout);
-
- private:
-  friend class HttpRequest;
-
-  HttpConnection(const std::shared_ptr<Base>& base, const std::string& host,
-                 unsigned short port);
-
-  const std::shared_ptr<Base> base_;
-  evhttp_connection* const conn_;
-
-  DISALLOW_COPY_AND_ASSIGN(HttpConnection);
 };
 
 
