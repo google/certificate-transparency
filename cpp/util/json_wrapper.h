1f7ca37b0cc4041793d04d9b5481be9c52dbba84
Ben Laurie <ben@links.org>
2015-04-11 15:40:29 +0100

    Remove redundant call to deprecated function json_object_object_get().

--- cpp/util/json_wrapper.h
+++ cpp/util/json_wrapper.h
@@ -119,7 +119,6 @@ class JsonObject {
  private:
   void InitFromChild(const JsonObject& from, const char* field,
                      json_type type) {
-    obj_ = json_object_object_get(from.obj_, field);
     if (json_object_object_get_ex(from.obj_, field, &obj_)) {
       if (!json_object_is_type(obj_, type)) {
         LOG(ERROR) << "Don't understand " << field
