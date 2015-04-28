e1bd1fbde2be4999e55b641739bec9bcf7ca39fc
Pierre Phaneuf <pphaneuf@google.com>
2015-04-28 16:56:26 +0100

    Detect whether pthread_t is a pointer or not.

--- cpp/server/ct-server.cc
+++ cpp/server/ct-server.cc
@@ -16,6 +16,8 @@
 #include <string>
 #include <unistd.h>
 
+
+#include "config.h"
 #include "log/cert_checker.h"
 #include "log/cert_submission_handler.h"
 #include "log/cluster_state_controller.h"
@@ -30,9 +32,9 @@
 #include "log/sqlite_db.h"
 #include "log/strict_consistent_store.h"
 #include "log/tree_signer.h"
+#include "monitoring/latency.h"
 #include "monitoring/monitoring.h"
 #include "monitoring/registry.h"
-#include "monitoring/latency.h"
 #include "server/handler.h"
 #include "server/json_output.h"
 #include "server/metrics.h"
@@ -383,9 +385,7 @@ void locking_function(int mode, int n, const char* file, int line) {
 
 
 void threadid_function(CRYPTO_THREADID* id) {
-  // FIXME(benl): platform differences should all live together somewhere,
-  // and ideally done by feature detection.
-#ifdef __FreeBSD__
+#ifdef PTHREAD_T_IS_POINTER
   CRYPTO_THREADID_set_pointer(id, pthread_self());
 #else
   CRYPTO_THREADID_set_numeric(id, pthread_self());
