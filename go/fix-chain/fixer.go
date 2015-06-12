f1c18c60f310cc9c5ccdb574dae2ef07f7791d2f
Ben Laurie <ben@links.org>
2015-06-12 18:25:54 +0100

    Missing file.

---
+++ go/fix-chain/fixer.go
@@ -0,0 +1,196 @@
+package fix_chain
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"github.com/google/certificate-transparency/go/x509"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
+)
+
+func Hash(s *x509.Certificate) ([sha256.Size]byte) {
+	return sha256.Sum256(s.Raw)
+}
+
+func HexHash(s *x509.Certificate) (string) {
+	h := Hash(s)
+	return hex.EncodeToString(h[:])
+}
+
+func dumpChain(name string, certs []*x509.Certificate) {
+	for i, cert := range certs {
+		log.Printf("%s %d: %s %s", name, i, HexHash(cert), cert.Subject.CommonName)
+	}
+}
+
+func dumpChains(name string, chains [][]*x509.Certificate) {
+	for i, chain := range chains {
+		n := fmt.Sprintf("%s %d", name, i)
+		dumpChain(n, chain)
+	}
+}
+
+func knownBad(name string) (bool) {
+	return name == "http://gca.nat.gov.tw/repository/Certs/IssuedToThisCA.p7b" || name == "http://grca.nat.gov.tw/repository/Certs/IssuedToThisCA.p7b" || name == "http://crt.trust-provider.com/AddTrustExternalCARoot.p7c" || name == "http://crt.usertrust.com/AddTrustExternalCARoot.p7c"
+}
+
+var URLCache map[string][]byte
+
+func GetURL(url string) (r []byte, err error) {
+	if URLCache == nil {
+		URLCache = make(map[string][]byte)
+	}
+	r, ok := URLCache[url]
+	if ok {
+		log.Printf("HIT! %s", url)
+	} else {
+		c, err0 := http.Get(url)
+		// FIXME: cache errors
+		if err0 != nil {
+			err = err0
+			return
+		}
+		defer c.Body.Close()
+		if c.StatusCode != 200 {
+			err = errors.New(fmt.Sprintf("can't deal with status %d", c.StatusCode))
+			return
+		}
+		r, err = ioutil.ReadAll(c.Body)
+		if err != nil {
+			return
+		}
+		URLCache[url] = r
+	}
+	return
+}
+
+type DedupedChain struct {
+	certs []*x509.Certificate
+}
+
+func (d *DedupedChain) AddCert(cert *x509.Certificate) {
+	// Check that the certificate isn't being added twice.
+	for _, c := range d.certs {
+		if c.Equal(cert) {
+			return
+		}
+	}
+	d.certs = append(d.certs, cert)
+}
+
+func (d *DedupedChain) Dump(name string) {
+	dumpChain(name, d.certs)
+}
+
+type Fix struct {
+	cert *x509.Certificate
+	chain *DedupedChain
+	opts *x509.VerifyOptions
+}
+
+func fixChain(fix *Fix, l *Log) {
+	d2 := *fix.chain
+	d2.AddCert(fix.cert)
+	for _, c := range d2.certs {
+		urls := c.IssuingCertificateURL
+		for i, url := range urls {
+			log.Printf("fetch issuer %d from %s", i, url)
+			body, err := GetURL(url)
+			if err != nil {
+				log.Printf("can't get URL body from %s: %s", url, err)
+				continue
+			}
+			//log.Print(body)
+			icert, err := x509.ParseCertificate(body)
+			if err != nil {
+				s, _ := pem.Decode(body)
+				if s != nil {
+					icert, err = x509.ParseCertificate(s.Bytes)
+				}
+			}
+			if err != nil {
+				if knownBad(url) {
+					log.Printf("(ignored) failed to parse certificate: %s", err)
+					continue
+				} else {
+					log.Fatalf("failed to parse certificate: %s", err)
+				}
+			}
+			//log.Printf("%+v", icert)
+			fix.opts.Intermediates.AddCert(icert)
+			chain, err := fix.cert.Verify(*fix.opts)
+			if err == nil {
+				dumpChains("fixed", chain)
+				l.PostChains(chain)
+				return
+			}
+		}
+	}
+	log.Printf("failed to fix certificate for %s", fix.cert.Subject.CommonName)
+}
+
+type Fixer struct {
+	fix chan *Fix
+	active uint16
+	wg sync.WaitGroup
+	log *Log
+}
+
+func (f *Fixer) fixChain(cert *x509.Certificate, d *DedupedChain, intermediates *x509.CertPool, l *Log) {
+	opts := x509.VerifyOptions{ Intermediates: intermediates, Roots: l.Roots() }
+	chain, err := cert.Verify(opts)
+	if err == nil {
+		dumpChains("verified", chain)
+		l.PostChains(chain)
+		return
+	}
+	log.Printf("failed to verify certificate for %s: %s", cert.Subject.CommonName, err)
+	f.deferFixChain(cert, d, &opts)
+}
+
+func (f *Fixer) FixAll(d *DedupedChain) {
+	intermediates := x509.NewCertPool()
+	for _, c := range d.certs {
+		intermediates.AddCert(c)
+	}
+	for _, c := range d.certs {
+		f.fixChain(c, d, intermediates, f.log)
+	}
+}
+
+// FIXME: make |log| a property of Fixer.
+func (f *Fixer) deferFixChain(cert *x509.Certificate, chain *DedupedChain, opts *x509.VerifyOptions) {
+	f.wg.Add(1)
+	f.fix <- &Fix{ cert: cert, chain: chain, opts: opts }
+}
+
+func (f *Fixer) fixServer() {
+	for {
+		fix := <-f.fix
+		f.active++
+		log.Printf("%d active fixers", f.active)
+		fixChain(fix, f.log)
+		f.active--
+		log.Printf("%d active fixers", f.active)
+		f.wg.Done()
+	}
+}
+
+func (f *Fixer) Wait() {
+	// Must wait for fixers first, in case they log something.
+	f.wg.Wait()
+	f.log.Wait()
+}
+
+func NewFixer(logurl string) (f *Fixer) {
+	f = &Fixer { fix: make(chan *Fix), log: NewLog(logurl) }
+	for i := 0 ; i < 100 ; i++ {
+		go f.fixServer()
+	}
+	return
+}
