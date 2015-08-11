9ab1b51fd4d964952287569ad8b4f5dd4ff3b6f0
Martin2112 <mhs@google.com>
2015-08-11 09:06:27 +0100

    Remove CMS from redaction test script as there is a separate script to build this test data.

--- test/generate_v2_unit_test_certs.sh
+++ test/generate_v2_unit_test_certs.sh
@@ -22,25 +22,6 @@ create_cert() {
     -out $pem_file -extfile $config -extensions $ext_section
 } 
 
-sign_cms() {
-  der_file=$1
-  sign_cert=$2
-  sign_key=$3
-
-  rm -f $der_file
-
-  cat > /tmp/cms_payload <<EOF
-THIS IS TEST DATA
------------------
-EOF
-
-  # -nocerts prevents the signing cert from being included and -nodetach
-  # includes the message content in the output
-  openssl cms -sign -outform DER -signer testdata/$sign_cert \
-    -inkey testdata/$sign_key -out $der_file -in /tmp/cms_payload \
-    -passin pass:password1 -binary -nodetach -nocerts
-}
-
 make_redaction_test_case() {
   test_name=$1
 
@@ -57,16 +38,6 @@ make_name_constraint_test_case() {
 
   create_cert testdata/v2/configs/constraint/${test_name}.config $1 \
     testdata/v2/constraint_${test_name}.pem
-}
-
-make_cms_test_case() {
-  test_name=$1
-  sign_cert=$2
-  sign_key=$3
-
-  echo $1 ':'
-
-  sign_cms testdata/v2/cms_${test_name}.der $sign_cert $sign_key
 }
 
 # Generate the redaction test cases
@@ -95,6 +66,3 @@ make_name_constraint_test_case test7
 make_name_constraint_test_case test8
 make_name_constraint_test_case test9
 
-# Generate the CMS test data
-
-make_cms_test_case ca_signed_noncert ca-cert.pem ca-key.pem
