2db4932dbf7ef31ffc5444792d6282e359591b3f
Al Cutter <al@google.com>
2015-09-16 11:14:47 +0100

    Retry failed operations during updates.

--- cloud/google/update_mirror.sh
+++ cloud/google/update_mirror.sh
@@ -7,25 +7,34 @@ fi
 source ${DIR}/util.sh
 source ${DIR}/config.sh $1
 
-set -e
 GCLOUD="gcloud"
 
 Header "Updating mirror instances..."
-for i in `seq 0 $((${MIRROR_NUM_REPLICAS} - 1))`; do
+i=0
+while [ $i -lt ${MIRROR_NUM_REPLICAS} ]; do
   echo "Updating ${MIRROR_MACHINES[${i}]}"
   echo "${MIRROR_META[${i}]}" > /tmp/metadata.${i}
 
-  gcloud compute instances add-metadata \
+  if ! gcloud compute instances add-metadata \
       ${MIRROR_MACHINES[${i}]} \
       --zone ${MIRROR_ZONES[${i}]} \
-      --metadata-from-file google-container-manifest=/tmp/metadata.${i}
+      --metadata-from-file google-container-manifest=/tmp/metadata.${i}; then
+    echo "Retrying"
+    continue
+  fi
+  
 
-  gcloud compute ssh ${MIRROR_MACHINES[${i}]} \
+  if ! gcloud compute ssh ${MIRROR_MACHINES[${i}]} \
       --zone ${MIRROR_ZONES[${i}]} \
       --command \
           'sudo docker pull gcr.io/'${PROJECT}'/super_mirror:test &&
-           sudo docker kill $(sudo docker ps | grep super_mirror | awk -- "{print \$1}" )'
+           sudo docker kill $(sudo docker ps | grep super_mirror | awk -- "{print \$1}" )'; then
+    echo "Retrying"
+    continue
+  fi
+
   WaitHttpStatus ${MIRROR_MACHINES[${i}]} ${MIRROR_ZONES[${i}]} /ct/v1/get-sth 200
+  i=$(($i + 1))
 done;
 
 
