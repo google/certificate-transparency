410f7b94438150931f3b01e10ec4713ea332d241
Rob Percival <robpercival@google.com>
2016-03-10 19:41:23 +0000

    Switch to using "cryptography" Python lib
    Replaces "ecdsa" and "pycrypto" libraries; the former is considered to be
    poorly implemented, whilst the latter is poorly maintained.
    
    See: https://cryptography.io
    
    Fixes some broken test

--- python/ct/crypto/verify_rsa.py
+++ python/ct/crypto/verify_rsa.py
@@ -2,9 +2,11 @@ from ct.crypto import error
 from ct.crypto import pem
 from ct.proto import client_pb2
 
-import Crypto.Hash.SHA256
-import Crypto.PublicKey.RSA
-import Crypto.Signature.PKCS1_v1_5
+import cryptography
+from cryptography.hazmat.backends import default_backend as crypto_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
 
 class RsaVerifier(object):
     """Verifies RSA signatures."""
@@ -14,11 +16,6 @@ class RsaVerifier(object):
     # The hash algorithm used for this public key.
     HASH_ALGORITHM = client_pb2.DigitallySigned.SHA256
 
-    # Markers to look for when reading a PEM-encoded RSA public key.
-    __READ_MARKERS = ("PUBLIC KEY", "RSA PUBLIC KEY")
-    # A marker to write when writing a PEM-encoded RSA public key.
-    __WRITE_MARKER = "RSA PUBLIC KEY"
-
     def __init__(self, key_info):
         """Creates a verifier that uses a PEM-encoded RSA public key.
 
@@ -27,22 +24,27 @@ class RsaVerifier(object):
 
         Raises:
         - PemError: If the key has an invalid encoding
+        - UnsupportedAlgorithmError: If the key uses an unsupported algorithm
         """
         if (key_info.type != client_pb2.KeyInfo.RSA):
             raise error.UnsupportedAlgorithmError(
                 "Expected RSA key, but got key type %d" % key_info.type)
 
-        # Will raise a PemError on invalid encoding
-        self.__der, _ = pem.from_pem(key_info.pem_key, self.__READ_MARKERS)
+        pem_key = str(key_info.pem_key)
+
         try:
-            self.__key = Crypto.PublicKey.RSA.importKey(self.__der)
-        except (ValueError, IndexError, TypeError) as e:
-            raise error.EncodingError(e)
+            self.__key = crypto_backend().load_pem_public_key(pem_key)
+        except ValueError as e:
+            raise pem.PemError(e)
+        except cryptography.exceptions.UnsupportedAlgorithm as e:
+            raise error.UnsupportedAlgorithmError(e)
 
     def __repr__(self):
-        return "%s(public key: %r)" % (self.__class__.__name__,
-                                       pem.to_pem(self.__der,
-                                                  self.__WRITE_MARKER))
+        key_pem = self.__key.public_bytes(
+            serialization.Encoding.PEM,
+            serialization.PublicFormat.SubjectPublicKeyInfo)
+
+        return "%s(public key: %r)" % (self.__class__.__name__, key_pem)
 
     @error.returns_true_or_raises
     def verify(self, signature_input, signature):
@@ -58,12 +60,13 @@ class RsaVerifier(object):
         Raises:
         - error.SignatureError: If the signature fails verification.
         """
-        verifier = Crypto.Signature.PKCS1_v1_5.new(self.__key)
-        sha256_hash = Crypto.Hash.SHA256.new(signature_input)
+        verifier = self.__key.verifier(signature, padding.PKCS1v15(), hashes.SHA256())
+        verifier.update(signature_input)
 
-        if verifier.verify(sha256_hash, signature):
-            return True
-        else:
-            raise error.SignatureError("Signature did not verify: %s",
+        try:
+            verifier.verify()
+        except cryptography.exceptions.InvalidSignature:
+            raise error.SignatureError("Signature did not verify: %s" %
                                        signature.encode("hex"))
 
+        return True
