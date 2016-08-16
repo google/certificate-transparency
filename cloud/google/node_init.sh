40cad23c3b63da58c84af04dd8825e1a10b044ec
Al Cutter <al@google.com>
2016-08-16 11:46:26 +0100

    update expected fluentd hash

--- cloud/google/node_init.sh
+++ cloud/google/node_init.sh
@@ -8,20 +8,21 @@ sudo /usr/share/google/safe_format_and_mount \
 
 # Install google-fluentd which pushes application log files up into the Google
 # Cloud Logs Monitor.
-EXPECTED_SHA256="48a50746b23c1deb7ec4b5e377631e2c77b95ecada5eda2bc8986a1e04359141  ./google-fluentd-install.sh"
-curl -sSO https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh
+AGENT_INSTALL_SCRIPT="install-logging-agent.sh"
+EXPECTED_SHA256="07ca6e522885b9696013aaddde48bf2675429e57081c70080a9a1364a411b395  ./${AGENT_INSTALL_SCRIPT}"
+curl -sSO https://dl.google.com/cloudagents/${AGENT_INSTALL_SCRIPT}
 if ! echo "${EXPECTED_SHA256}" | sha256sum --quiet -c; then
-  echo "Got google-fluentd-install.sh with sha256sum "
-  sha256sum ./google-fluentd-install.sh
+  echo "Got ${AGENT_INSTALL_SCRIPT} with sha256sum "
+  sha256sum ./${AGENT_INSTALL_SCRIPT}
   echo "But expected:"
   echo "${EXPECTED_SHA256}"
-  echo "google-fluentd-install.sh may have been updated, verify the new sum at"
+  echo "${AGENT_INSTALL_SCRIPT} may have been updated, verify the new sum at"
   echo "https://cloud.google.com/logging/docs/agent/installation and update"
   echo "this script with the new sha256sum if necessary."
   exit 1
 fi
 
-sudo bash ./google-fluentd-install.sh
+sudo bash ./${AGENT_INSTALL_SCRIPT}
 cat > /tmp/ct-info.conf <<EOF
 <source>
   type tail
