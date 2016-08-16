6eb2f0105f893ac8b623f12dbb8d115801e04450
Al Cutter <al@google.com>
2016-08-16 11:44:43 +0100

    disable fetching from for the certly mirror

--- cloud/google/configs/ct-mirror-certly1.sh
+++ cloud/google/configs/ct-mirror-certly1.sh
@@ -4,6 +4,6 @@ export REGION="us-central1"
 export CLUSTER="ct-mirror-certly1"
 export ZONES="a c f"
 export MIRROR_NUM_REPLICAS_PER_ZONE=2
-export MIRROR_TARGET_URL="https://log.certly.io"
+export MIRROR_TARGET_URL="" # target is down, was https://log.certly.io
 export MIRROR_TARGET_PUBLIC_KEY="certly_1.pem"
 export MONITORING="gcm"
